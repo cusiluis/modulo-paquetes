aab12557c1591e0043193ce673caaaaeaae163f2
Dimelza Paola Salazar Apaza <paodi@MBP-de-Dimelza.local>
2025-07-28 04:04:53 -0400

    opciones

--- app/views/admin/includes/sidebar.php
+++ app/views/admin/includes/sidebar.php
@@ -4,7 +4,7 @@ include APP_ROOT . '/app/core/globales.inc.php';
 ?>
  <aside class="app-sidebar bg-body-secondary shadow" data-bs-theme="dark"> <!--begin::Sidebar Brand-->
       <div class="sidebar-brand"> <!--begin::Brand Link--> <a href="../index.html" class="brand-link">
-          <!--begin::Brand Image--> <img src="<?php echo BASE_URL; ?>public/adminlte/assets/img/AdminLTELogo.png" alt="Reinovip Logo"
+          <!--begin::Brand Image--> <img src="<?php echo BASE_URL; ?>public/adminlte/assets/img/logo-administrador.png" alt="Administrador"
             class="brand-image opacity-75 shadow"> <!--end::Brand Image--> <!--begin::Brand Text--> <span
             class="brand-text fw-light">Reinovip</span> <!--end::Brand Text--> </a> <!--end::Brand Link--> </div>
       <!--end::Sidebar Brand--> <!--begin::Sidebar Wrapper-->
@@ -18,28 +18,26 @@ include APP_ROOT . '/app/core/globales.inc.php';
                 </p>
               </a>
             </li>
-            <li class="nav-item"> <a href="<?php echo BASE_URL; ?>admin/usuarios" class="nav-link"> <i class="nav-icon bi bi-circle text-danger"></i>
+            <li class="nav-item"> <a href="<?php echo BASE_URL; ?>admin/usuarios" class="nav-link"> <i class="fa-solid fa-user-pen"></i>
                 <p class="text">Usuarios</p>
               </a> </li>
-            <li class="nav-item"> <a href="#" class="nav-link"> <i class="nav-icon bi bi-circle text-warning"></i>
+            <li class="nav-item"> <a href="#" class="nav-link"> <i class="fa-solid fa-boxes-packing"></i>
                 <p>Gestion de Paquetes</p>
               </a> 
                <ul class="nav nav-treeview">
-                <li class="nav-item"> <a href="<?php echo BASE_URL; ?>admin/paquetes" class="nav-link"> <i class="nav-icon bi bi-circle"></i>
+                <li class="nav-item"> <a href="<?php echo BASE_URL; ?>admin/paquetes" class="nav-link"> <i class="fa-solid fa-box-archive"></i>
                     <p>Paquetes</p>
                   </a> </li>
-                <li class="nav-item"> <a href="<?php echo BASE_URL; ?>paquetes/listar_opciones" class="nav-link"> <i class="nav-icon bi bi-circle"></i>
+                <li class="nav-item"> <a href="<?php echo BASE_URL; ?>paquetes/listar_opciones" class="nav-link"> <i class="fa-solid fa-cubes"></i>
                     <p>Opciones de Paquetes</p>
                   </a> </li>
-                  <li class="nav-item"> <a href="<?php echo BASE_URL; ?>paquetes/listar_solicitudes" class="nav-link"> <i class="nav-icon bi bi-circle"></i>
+                  <li class="nav-item"> <a href="<?php echo BASE_URL; ?>paquetes/listar_solicitudes" class="nav-link"> <i class="fa-solid fa-box-open"></i>
                     <p>Solicitudes de Paquetes</p>
                   </a> </li>
 
               </ul>
             </li>
-            <li class="nav-item"> <a href="#" class="nav-link"> <i class="nav-icon bi bi-circle text-info"></i>
-                <p>Informational</p>
-              </a> </li>
+            
           </ul> <!--end::Sidebar Menu-->
         </nav>
       </div> <!--end::Sidebar Wrapper-->
